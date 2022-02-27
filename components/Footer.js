a8d0e297009ea025967574fb20b17e44e85aa011
Rebecca Davidsson <rebeccadavidsson3@gmail.com>
2022-02-27 15:39:25 +0100

    Change <a to <Link

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,5 @@
 import NextImage from "./Image"
+import Link from "next/link"
 
 const Footer = () => {
   return (
@@ -7,23 +8,20 @@ const Footer = () => {
         Philip Davidsson - 2022
       </p>
       <div className="flex gap-3 ml-4">
-        <a href="/" className="max-w-xs ml-4">
+        <Link href="/" className="max-w-xs ml-4" passHref>
           <NextImage src="/twitter.svg" width={20} height={20} alt="Twitter" />
-        </a>
-        <a href="/" className="ml-3">
+        </Link>
+        <Link href="/" className="ml-3" passHref>
           <NextImage
             src="/facebook.svg"
             width={20}
             height={20}
             alt="Facebook"
           />
-        </a>
-        <a
-          href="/"
-          className="ml-3"
-        >
+        </Link>
+        <Link href="/" className="ml-3" passHref>
           <NextImage src="/github.svg" width={20} height={20} alt="GitHub" />
-        </a>
+        </Link>
       </div>
     </div>
   )
